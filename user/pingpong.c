f66cbb43cd32454eee2f79120762a74662a89e04
PeterHUistyping <2447276617@qq.com>
2023-03-19 16:17:08 +0000

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,11 +3,23 @@
 #include "user/user.h"
 
 int main(int argc,char* argv[]){
-    if(argc!=2){
-        // printf("%d",argc);
-        fprintf(2,"usage: sleep time\n");
-        exit(1);
+    int pid=fork();
+    int fds[2];
+    pipe(fds);
+    char buffer[100];
+    int n;
+    if(pid<0){
+        return 1;
     }
-    sleep(atoi(argv[1]));
+    else if(pid==0){
+        write(fds[1],"1",1);
+    }
+    else{
+        int pid;
+        n=read(fds[0],buffer,sizeof(fds));
+        if(n>0){
+            printf("%d:received ping",pid);
+        }
+    }
     exit(0);
 }
