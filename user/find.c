58c1f43d63f2b602b8ba3fe4d42758e2a075533f
PeterHUistyping <2447276617@qq.com>
2023-03-19 23:38:04 +0000

    find from ls

--- user/find.c
+++ user/find.c
@@ -1,53 +1,79 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
+ 
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
 
-void filter(int * fds){
-    int fds2[2]={0};
-    pipe(fds2);
-    int primes[35]={0};
-    int index=0;
-    while(read(fds[0],&primes[index],sizeof(index))){  // read turns 0 when the write side of a pipe is closed.
-        index++;
-        // printf("read %d\n",primes[index]);
-    }
-    if(index==0){
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void find(char *path, char *word ){
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "ls: cannot open %s\n", path);
         return;
     }
-    close(fds[0]);
-    printf("prime %d\n",primes[0]);
 
-    // if(primes[0]==35){
-    //     return;
-    // }
-    for(int i=1;i<index;i++){
-        
-        if(primes[i]%primes[0]==0){
-            //printf("continue %d\n",primes[i]);
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "ls: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    switch(st.type){
+    case T_DEVICE:
+    case T_FILE:
+        printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+    break;
+
+    case T_DIR:
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+        printf("ls: path too long\n");
+        break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+        if(de.inum == 0)
+        continue;
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if(stat(buf, &st) < 0){
+            printf("ls: cannot stat %s\n", buf);
             continue;
         }
-        //printf("Write %d\n",primes[i]);
-        write(fds2[1],&primes[i],sizeof(i));
+        printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
     }
-    close(fds2[1]);
-    int pid=fork();
-    if(pid==0){ //child
-        filter(fds2);
+    break;
     }
-    else{
-        //SHOULDN'T BE HERE
-    }
+    close(fd);
 }
+
+
 int main(int argc,char* argv[]){
-    int fds[2];
-    
-    pipe(fds);
-    
-    for(int i=2;i<=35;i++){
-        write(fds[1],&i,sizeof(i));
-    }
-    close(fds[1]);
-    filter(fds);
-    wait((int *)0); // wait the entire pipe finished.
+    // check parameter = 3
+    // check argv[1] is dir
+    find(argv[1],argv[2]);
     exit(0);
+    
 }
